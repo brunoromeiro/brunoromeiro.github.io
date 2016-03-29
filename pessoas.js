5327415f01566d1231e625c3f1d2a16f7d238454
brunoromeiro <bruno.romeiro@gmail.com>
2016-03-29 14:31:27 -0300

    Update pessoas.js

--- pessoas.js
+++ pessoas.js
@@ -1,3 +1,30 @@
-$.getJSON('pessoas.json',function(data){
-
+//Classe para chamar o Json
+function json(){
+  var qtd;
+  var retorno;
+  
+  //Resgatar valores
+  json.prototype.resgatarValores = function(){
+    $('#resultado').html('Carregando dados...');
+    
+    //Estrutura de Resultado
+    $.getJSON('pessoas.json', function(data){
+      this.qtd = data.pessoas.length;
+      this.retorno = '';
+      
+      for (i=0; i<this.qtd; i++){
+        this.retorno += 'NOME: ' + data.pessoas[i].nome + '<br />';
+        this.retorno += 'EMAIL: ' + data.pessoas[i].email + ' - ';
+        this.retorno += 'CIDADE: ' + data.pessoas[i]cidade + '<br /><br />';
+      }
+      
+      $('#resultado').html(this.retorno);
+    });
+    
+  }
+  
 }
+
+//Objeto
+var obj = new json();
+obj.resgatarValores();
