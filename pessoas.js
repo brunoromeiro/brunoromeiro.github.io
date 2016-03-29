20bd0e138417d7ee06c2f122dc4f4ec67add9397
brunoromeiro <bruno.romeiro@gmail.com>
2016-03-29 15:31:01 -0300

    Update pessoas.js

--- pessoas.js
+++ pessoas.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('#tabela').empty(); //limpando a tabela
 	$.ajax({
-		dataType: 'json', //Definimos o tipo de retorno
 		url: 'pessoas.json', //Definindo o arquivo de onde serão buscados os dados
 		success: function(dados){
 			for(var i=0;dados.length>i;i++){
